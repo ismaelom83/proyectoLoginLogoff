0bc9699467369f40a928a8cfa90c93969ec478a0
ismael <ismaelom83@gmail.es>
2019-11-28 18:04:05 +0100

    MASTER 191127.b

--- login.php
+++ login.php
@@ -8,7 +8,7 @@
         <title>Mostrar Departamentos</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="../WEBBROOT/css/estilosmostrar.css">
+        <link rel="stylesheet" href="WEBBROOT/css/estilosmostrar.css">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     </head>
     <header>
